ccbb01aa175554f19bae25d0da5c0fae53ab7417
Sebastián Soto <spsotor@gmail.com>
2018-07-21 20:37:02 -0400

    Some other linked list methods

--- linked_list.go
+++ linked_list.go
@@ -63,3 +63,50 @@ func (l *LinkedList) Delete(e *LinkedListElement) {
 		e.next.prev = e.prev // If it's nil, is propagated
 	}
 }
+
+// Swap positions of two elements in a linked list.
+func (l *LinkedList) Swap(x *LinkedListElement, y *LinkedListElement) {
+	if x.prev == nil {
+		// It's the head. Make the swap.
+		l.head = y
+	} else {
+		// The next of the previous is the new item.
+		x.prev.next = y
+	}
+
+	if y.prev == nil {
+		// It's the head. Make the swap.
+		l.head = x
+	} else {
+		// The next of the previous is the new item.
+		y.prev.next = x
+	}
+
+	if x.next != nil {
+		// The previous of the next is the new item.
+		x.next.prev = y
+	}
+
+	if y.next != nil {
+		// The previous of the next is the new item.
+		y.next.prev = x
+	}
+
+	// Now swap the pointers on the final item.
+	x.prev, x.next, y.prev, y.next = y.prev, y.next, x.prev, x.next
+}
+
+// Reverse reverses the order of a linked list.
+//
+// `O(n)` complexity.
+func (l *LinkedList) Reverse() {
+	e := l.head
+
+	for e != nil {
+		if e.next == nil {
+			l.head = e
+		}
+
+		e.next, e.prev, e = e.prev, e.next, e.next
+	}
+}
